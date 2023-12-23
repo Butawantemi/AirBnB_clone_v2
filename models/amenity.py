a9cb409372b1bf111907103d0baa9c4246a0d7ab
Butawantemi <japhetbuta@gmail.com>
2023-12-23 22:21:43 +0300

    AirBnB_clone_v2

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+""" State Module """
+from models.base_model import BaseModel, Base
+import os
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.place import place_amenity
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """Represent amenity"""
+    __tablename__ = 'amenities'
+    name = Column(
+        String(128), nullable=False
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+
+    place_amenities = relationship(
+        "Place", secondary=place_amenity, backref="amenity")
